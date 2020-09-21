1c47b8b605d3d2101952cd82a4ac2d3d3f7a3fdf
Karol Wolski <poczta@karol-wolski.pl>
2020-09-21 12:17:15 +0200

    fix: console error when page does not have carousel (#6)

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,14 +2,17 @@ let slideIndex = 0
 
 function showSlides() {
   const slides = document.getElementsByClassName('carousel__item')
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove('carousel__item--active')
-  }
-  slideIndex++
-  if (slideIndex > slides.length) slideIndex = 1
 
-  slides[slideIndex - 1].classList.add('carousel__item--active')
-  setTimeout(showSlides, 5000)
+  if (slides.length > 0) {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('carousel__item--active')
+    }
+    slideIndex++
+    if (slideIndex > slides.length) slideIndex = 1
+
+    slides[slideIndex - 1].classList.add('carousel__item--active')
+    setTimeout(showSlides, 5000)
+  }
 }
 
 export default showSlides
